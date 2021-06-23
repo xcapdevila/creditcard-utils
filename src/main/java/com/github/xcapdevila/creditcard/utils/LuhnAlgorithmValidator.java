07e34be62344e3441cbca6d6c5a9795f7aada03c
Xavier Capdevila <xavier.capdevila@fonyou.com>
2021-06-23 20:01:31 +0200

    Update Luhn validator to check if pan is only digits

--- src/main/java/com/github/xcapdevila/creditcard/utils/LuhnAlgorithmValidator.java
+++ src/main/java/com/github/xcapdevila/creditcard/utils/LuhnAlgorithmValidator.java
@@ -1,5 +1,7 @@
 package com.github.xcapdevila.creditcard.utils;
 
+import java.util.regex.Pattern;
+
 /**
  * @author Xavier Capdevila Estevez on 27/5/21.
  */
@@ -8,19 +10,18 @@ public class LuhnAlgorithmValidator {
   private static final int LUHN_LAST_DIGIT = 9;
   private static final int LUHN_MULTIPLIER = 2;
   private static final long LUHN_MODULE = 10L;
+  private static final Pattern ONLY_DIGITS = Pattern.compile("\\d+");
 
   public boolean isValid(final String pan) {
+    if (!ONLY_DIGITS.matcher(pan).matches()) {
+      throw new RuntimeException("Luhn requires an only digits pan to be validated");
+    }
     int digits = pan.length();
     int oddOrEven = digits & 1;
     long sum = 0L;
 
     for (int count = 0; count < digits; ++count) {
-      int digit;
-      try {
-        digit = Integer.parseInt(String.valueOf(pan.charAt(count)));
-      } catch (NumberFormatException exception) {
-        throw new RuntimeException(exception);
-      }
+      int digit = Integer.parseInt(String.valueOf(pan.charAt(count)));
 
       if (((count & 1) ^ oddOrEven) == 0) {
         digit *= LUHN_MULTIPLIER;
--- src/test/java/com/github/xcapdevila/creditcard/utils/LuhnAlgorithmValidatorTests.java
+++ src/test/java/com/github/xcapdevila/creditcard/utils/LuhnAlgorithmValidatorTests.java
@@ -1,6 +1,7 @@
 package com.github.xcapdevila.creditcard.utils;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,7 @@ class LuhnAlgorithmValidatorTests {
 
   private static final String VALID_PAN = "340184587300287";
   private static final String INVALID_PAN = "340184587300283";
+  private static final String MALFORMED_PAN = "34018A5873E0283";
   private LuhnAlgorithmValidator luhnAlgorithmValidator;
 
   @BeforeEach
@@ -25,6 +27,11 @@ class LuhnAlgorithmValidatorTests {
   @Test
   void givenAnInvalidCreditCardLuhnIsInvalid() {
     assertFalse(luhnAlgorithmValidator.isValid(INVALID_PAN));
+  }
+
+  @Test
+  void givenAMalformedCreditCardLuhnThrowsException() {
+    assertThrows(RuntimeException.class, () -> luhnAlgorithmValidator.isValid(MALFORMED_PAN));
   }
 
 }
