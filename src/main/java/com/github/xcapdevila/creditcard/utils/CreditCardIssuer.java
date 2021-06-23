27c5eda907aed676010b0af8e704f5b6dacc271e
Xavier Capdevila <xavier.capdevila@fonyou.com>
2021-06-23 19:38:32 +0200

    Add CreditCardIssuer with constructor validations

---
+++ src/main/java/com/github/xcapdevila/creditcard/utils/CreditCardIssuer.java
@@ -0,0 +1,61 @@
+package com.github.xcapdevila.creditcard.utils;
+
+import java.util.Objects;
+import lombok.Builder;
+import lombok.Getter;
+import org.junit.platform.commons.util.StringUtils;
+
+/**
+ * @author Xavier Capdevila Estevez on 20/6/21.
+ */
+@Builder(builderMethodName = "create", builderClassName = "Builder")
+public final class CreditCardIssuer {
+
+  @Getter
+  private final String name;
+  @Getter
+  private final Integer cards;
+  @Getter
+  private final String panRegex;
+  @Getter
+  private final String cvvRegex;
+  @Getter
+  private final String expDateRegex;
+
+  private final LuhnCompliance luhnCompliant;
+
+  private CreditCardIssuer(String name, Integer cards, String panRegex, String cvvRegex, String expDateRegex,
+      LuhnCompliance luhnCompliant) {
+
+    if (StringUtils.isBlank(name)) {
+      throw new RuntimeException("'name' is blank");
+    }
+    if (Objects.isNull(cards) || cards < 1) {
+      throw new RuntimeException("'cards' is lower than 1");
+    }
+    if (StringUtils.isBlank(panRegex)) {
+      throw new RuntimeException("'panRegex' is blank");
+    }
+    if (StringUtils.isBlank(cvvRegex)) {
+      throw new RuntimeException("'cvvRegex' is blank");
+    }
+    if (StringUtils.isBlank(expDateRegex)) {
+      throw new RuntimeException("'expDateRegex' is blank");
+    }
+    if (Objects.isNull(luhnCompliant)) {
+      throw new RuntimeException("'luhnCompliant' is null");
+    }
+
+    this.name = name;
+    this.cards = cards;
+    this.panRegex = panRegex;
+    this.cvvRegex = cvvRegex;
+    this.expDateRegex = expDateRegex;
+    this.luhnCompliant = luhnCompliant;
+  }
+
+  public boolean isLuhnCompliant(final String pan) {
+    return luhnCompliant.validate(pan);
+  }
+
+}
